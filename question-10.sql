b773b0737d0eba114ce2fc21b825276d3d43edf1
Mia Truong <tmanh1312@gmail.com>
2023-04-06 19:22:19 -0700

    q10 update

--- question-10.sql
+++ question-10.sql
@@ -1,2 +1,2 @@
 INSERT INTO persons (first_name, last_name, email, gender, title, age, address, city, state, postalcode, phone)
-VALUES ('Mia', 'Truong', 'tmanh131@uw.edu', 'Female', 'Ms', '23', '5 Union Park', 'Seattle', 'Washington', '98118', '253-414-3682');
+VALUES ('Mia', 'Truong', 't131@uw.edu', 'Female', 'Ms', '23', '5 Union Park', 'Seattle', 'Washington', '99999', '123-456-7899');
